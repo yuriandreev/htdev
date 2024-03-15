28d5f7286a9da6ee41b3ab7694c0cf064a6261d3
Yuri  A <mail@yuriandreev.com>
2024-03-15 23:24:15 +0400

    HTDev Init

---
+++ header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1">
+    <?php wp_head(); ?>
+
+
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<header>
+    <div class="wrapper">
+        <div class="header__menu">
+
+        </div>
+    </div>
+</header>
+
